dd9fa4656a578151c160fb9a4d42f5cc4d8c3c89
Gosia:) <msteperska@gmail.com>
2023-04-17 08:46:31 +0200

    master

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,187 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Add_button = new System.Windows.Forms.Button();
-            this.Remove_button = new System.Windows.Forms.Button();
-            this.SaveCSV_button = new System.Windows.Forms.Button();
-            this.LoadCSV_button = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.Title = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Author = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Genres = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Stars = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Search_button = new System.Windows.Forms.Button();
-            this.SaveXML_button = new System.Windows.Forms.Button();
-            this.LoadXML_button = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.load_button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // Add_button
+            // pictureBox1
             // 
-            this.Add_button.Location = new System.Drawing.Point(110, 399);
-            this.Add_button.Name = "Add_button";
-            this.Add_button.Size = new System.Drawing.Size(97, 35);
-            this.Add_button.TabIndex = 0;
-            this.Add_button.Text = "ADD";
-            this.Add_button.UseVisualStyleBackColor = true;
-            this.Add_button.Click += new System.EventHandler(this.Add_button_Click);
+            this.pictureBox1.Location = new System.Drawing.Point(66, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(272, 198);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
-            // Remove_button
+            // load_button
             // 
-            this.Remove_button.Location = new System.Drawing.Point(110, 453);
-            this.Remove_button.Name = "Remove_button";
-            this.Remove_button.Size = new System.Drawing.Size(97, 35);
-            this.Remove_button.TabIndex = 1;
-            this.Remove_button.Text = "REMOVE";
-            this.Remove_button.UseVisualStyleBackColor = true;
-            this.Remove_button.Click += new System.EventHandler(this.Remove_button_Click);
-            // 
-            // SaveCSV_button
-            // 
-            this.SaveCSV_button.Location = new System.Drawing.Point(450, 390);
-            this.SaveCSV_button.Name = "SaveCSV_button";
-            this.SaveCSV_button.Size = new System.Drawing.Size(129, 52);
-            this.SaveCSV_button.TabIndex = 2;
-            this.SaveCSV_button.Text = "SAVE TO .CSV\r\n";
-            this.SaveCSV_button.UseVisualStyleBackColor = true;
-            this.SaveCSV_button.Click += new System.EventHandler(this.Save_button_Click);
-            // 
-            // LoadCSV_button
-            // 
-            this.LoadCSV_button.Location = new System.Drawing.Point(610, 390);
-            this.LoadCSV_button.Name = "LoadCSV_button";
-            this.LoadCSV_button.Size = new System.Drawing.Size(134, 52);
-            this.LoadCSV_button.TabIndex = 3;
-            this.LoadCSV_button.Text = "LOAD FROM .CSV";
-            this.LoadCSV_button.UseVisualStyleBackColor = true;
-            this.LoadCSV_button.Click += new System.EventHandler(this.Load_button_Click);
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Title,
-            this.Author,
-            this.Genres,
-            this.Stars,
-            this.Price});
-            this.dataGridView1.Location = new System.Drawing.Point(43, 31);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(814, 332);
-            this.dataGridView1.TabIndex = 4;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
-            // 
-            // Title
-            // 
-            this.Title.HeaderText = "TITLE";
-            this.Title.MinimumWidth = 8;
-            this.Title.Name = "Title";
-            this.Title.ReadOnly = true;
-            this.Title.Width = 150;
-            // 
-            // Author
-            // 
-            this.Author.HeaderText = "AUTHOR";
-            this.Author.MinimumWidth = 8;
-            this.Author.Name = "Author";
-            this.Author.ReadOnly = true;
-            this.Author.Width = 150;
-            // 
-            // Genres
-            // 
-            this.Genres.HeaderText = "GENRES";
-            this.Genres.MinimumWidth = 8;
-            this.Genres.Name = "Genres";
-            this.Genres.ReadOnly = true;
-            this.Genres.Width = 150;
-            // 
-            // Stars
-            // 
-            this.Stars.HeaderText = "STARS";
-            this.Stars.MinimumWidth = 8;
-            this.Stars.Name = "Stars";
-            this.Stars.ReadOnly = true;
-            this.Stars.Width = 150;
-            // 
-            // Price
-            // 
-            this.Price.HeaderText = "PRICE";
-            this.Price.MinimumWidth = 8;
-            this.Price.Name = "Price";
-            this.Price.ReadOnly = true;
-            this.Price.Width = 150;
-            // 
-            // Search_button
-            // 
-            this.Search_button.Location = new System.Drawing.Point(275, 428);
-            this.Search_button.Name = "Search_button";
-            this.Search_button.Size = new System.Drawing.Size(97, 34);
-            this.Search_button.TabIndex = 5;
-            this.Search_button.Text = "SEARCH";
-            this.Search_button.UseVisualStyleBackColor = true;
-            this.Search_button.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // SaveXML_button
-            // 
-            this.SaveXML_button.Location = new System.Drawing.Point(450, 463);
-            this.SaveXML_button.Name = "SaveXML_button";
-            this.SaveXML_button.Size = new System.Drawing.Size(129, 54);
-            this.SaveXML_button.TabIndex = 6;
-            this.SaveXML_button.Text = "SAVE TO .XML";
-            this.SaveXML_button.UseVisualStyleBackColor = true;
-            this.SaveXML_button.Click += new System.EventHandler(this.SaveXML_button_Click);
-            // 
-            // LoadXML_button
-            // 
-            this.LoadXML_button.Location = new System.Drawing.Point(610, 463);
-            this.LoadXML_button.Name = "LoadXML_button";
-            this.LoadXML_button.Size = new System.Drawing.Size(134, 54);
-            this.LoadXML_button.TabIndex = 7;
-            this.LoadXML_button.Text = "LOAD FROM .XML";
-            this.LoadXML_button.UseVisualStyleBackColor = true;
-            this.LoadXML_button.Click += new System.EventHandler(this.LoadXML_button_Click);
+            this.load_button.Location = new System.Drawing.Point(66, 279);
+            this.load_button.Name = "load_button";
+            this.load_button.Size = new System.Drawing.Size(138, 46);
+            this.load_button.TabIndex = 1;
+            this.load_button.Text = "Wczytaj";
+            this.load_button.UseVisualStyleBackColor = true;
+            this.load_button.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(909, 529);
-            this.Controls.Add(this.LoadXML_button);
-            this.Controls.Add(this.SaveXML_button);
-            this.Controls.Add(this.Search_button);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.LoadCSV_button);
-            this.Controls.Add(this.SaveCSV_button);
-            this.Controls.Add(this.Remove_button);
-            this.Controls.Add(this.Add_button);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.load_button);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button Add_button;
-        private System.Windows.Forms.Button Remove_button;
-        private System.Windows.Forms.Button SaveCSV_button;
-        private System.Windows.Forms.Button LoadCSV_button;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Title;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Author;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Genres;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Stars;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Price;
-        private System.Windows.Forms.Button Search_button;
-        private System.Windows.Forms.Button SaveXML_button;
-        private System.Windows.Forms.Button LoadXML_button;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button load_button;
     }
 }
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -2,198 +2,38 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
-using System.IO;
 using System.Linq;
-using System.Reflection;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Xml;
-using System.Xml.Serialization;
 
 namespace WindowsFormsApp1
 {
-    [Serializable]
     public partial class Form1 : Form
     {
-        int index;
-        int row_counter = 0;
-        public Book book1 = new Book();
-        public List<Book> Book_list = new List<Book> ();
-
-        //save and load 
-        private SaveFileDialog sfd;
         private OpenFileDialog ofd;
         public Form1()
         {
             InitializeComponent();
         }
-               
-        public void Set_data()
-        {
-            Book_list.Add(new Book(book1.title, book1.author, book1.genres, book1.stars, book1.price));
-            dataGridView1.Rows.Add(Book_list.Last().title, Book_list.Last().author, Book_list.Last().genres, Book_list.Last().stars, Book_list.Last().price);
-            //dataGridView1.Rows.Add(title, author, genres, stars, price);
-            row_counter++;
-        }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void pictureBox1_Click(object sender, EventArgs e)
         {
 
-        }
-
-        private void Add_button_Click(object sender, EventArgs e)
-        {
-            Form2 form = new Form2(this);
-            form.ShowDialog();
-        }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-            index = e.RowIndex;
-        }
-
-        private void Remove_button_Click(object sender, EventArgs e)
-        {
-            if(row_counter != 0)
-            {
-                Book_list.RemoveAt(index);
-                dataGridView1.Rows.RemoveAt(index);
-                row_counter--;
-            }
-            
-        }
-
-        private void Save_button_Click(object sender, EventArgs e)
-        {
-            sfd = new SaveFileDialog();
-            sfd.Filter = ".csv | .csv"; 
-            if (sfd.ShowDialog() == DialogResult.OK)
-            {
-                string path = sfd.FileName; //tu zapisuje się ścieżka do pliku
-                this.dataGridView1.SelectAll();
-                string full_text = "";
-                
-                if (!File.Exists(path))
-                {
-                    for(int j = 0; j < dataGridView1.RowCount; j++)
-                    {
-                        for(int i = 0; i < 5; i++)
-                        {
-                            full_text += dataGridView1.Rows[j].Cells[i].Value.ToString(); 
-                            if(i == 4) {
-                                continue;
-                            }
-                            else {
-                                full_text += ",";
-                            }
-                            
-                        }
-                        if(j == dataGridView1.RowCount - 1) {
-                            continue;
-                        }
-                        else { 
-                            full_text += Environment.NewLine;
-                        }
-                        
-                        
-                    }
-                    File.WriteAllText(path, full_text);
-                }
-
-
-            }
-        }
-
-        private void Load_button_Click(object sender, EventArgs e)
-        {
-            //string title = "";
-            //string author = "";
-            //string genres = "";
-            //string stars = "";
-            //string price = "";
-            ofd = new OpenFileDialog();
-            ofd.ShowDialog();
-            if(ofd.ShowDialog() == DialogResult.OK)
-            {
-                string path = ofd.FileName; //ścieżka do pliku 
-                foreach(string line in System.IO.File.ReadLines(path))
-                {
-                    string[] data = line.Split(',');
-                    book1.title = data[0];
-                    book1.author = data[1];
-                    book1.genres = data[2];
-                    book1.stars = data[3];
-                    book1.price = data[4];
-                    Book_list.Add(new Book(book1.title, book1.author, book1.genres, book1.stars, book1.price));
-                    dataGridView1.Rows.Add(Book_list.Last().title, Book_list.Last().author, Book_list.Last().genres, Book_list.Last().stars, Book_list.Last().price);            
-                    row_counter++;
-                }
-               
-            }
-            
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {
-            Form3 form = new Form3(this);
-            form.ShowDialog();
-        }
-
-        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
-        {
-
-        }
-
-        private void SaveXML_button_Click(object sender, EventArgs e)
-        {            
-            
-            sfd = new SaveFileDialog();
-            sfd.Filter = ".xml | .xml";
-            if (sfd.ShowDialog() == DialogResult.OK)
-            {
-                string path = sfd.FileName;
-                if(!File.Exists(path)) 
-                {
-                    XmlSerializer ser = new XmlSerializer(typeof(List<Book>));
-                    StreamWriter writer = new StreamWriter(path);
-                    ser.Serialize(writer, Book_list);
-                    writer.Close();
-                }
-                
-            }
-                
-
-        }
-
-        private void LoadXML_button_Click(object sender, EventArgs e)
         {
             ofd = new OpenFileDialog();
             ofd.ShowDialog();
             if (ofd.ShowDialog() == DialogResult.OK)
             {
-                string path = ofd.FileName; //ścieżka do pliku 
-                XmlSerializer ser = new XmlSerializer(typeof(List<Book>));
-                StreamReader reader= new StreamReader(path);
-                Book_list = (List<Book>)ser.Deserialize(reader);
-                reader.Close();
-                while(dataGridView1.Rows.Count > 0)
-                {
-                    dataGridView1.Rows.RemoveAt(0);
-                }
-                for(int i = 0; i < Book_list.Count; i++)
-                {
-                    //????
-                    Book_list.Add(new Book(book1.title, book1.author, book1.genres, book1.stars, book1.price));
-                    dataGridView1.Rows.Add(Book_list.Last().title, Book_list.Last().author, Book_list.Last().genres, Book_list.Last().stars, Book_list.Last().price);
-                    row_counter++;
-                }
-
+                string path = ofd.FileName; //ścieżka do pliku
+                Bitmap bmp = new Bitmap(path);
+                pictureBox1.Image = bmp;
             }
-
-           
         }
     }
 }
